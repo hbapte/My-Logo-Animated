0bc2e1e7bd3e602378cc0b81d4476e80612c2111
hbapte <ijbapte@gmail.com>
2023-11-11 12:28:29 +0200

    MY LOGO - Animated

---
+++ main.js
@@ -0,0 +1,8 @@
+anime({
+    targets: '#logo-svg path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutQuad',
+    duration: 2500,
+    // loop: true,
+    direction: 'normal'
+})
